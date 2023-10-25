2dff9db49319cb4596cc46832cc512f7e0c36254
Vladislav Kalyuzhny <dalvvlad2004@mail.ru>
2023-10-25 11:28:44 +0300

    settings

--- AI.py
+++ AI.py
@@ -1,1 +1,20 @@
-print("Hello world")
+import telebot
+
+bot = telebot.TeleBot('6718573593:AAHVZng3giJHCTsO9m7ieF7qa3Z22Cghp3Y')
+
+
+
+@bot.message_handler(commands=['start'])
+def main(message):
+    bot.send_message(message.chat.id,f"Привет! {message.from_user.first_name} {message.from_user.last_name}")
+
+
+@bot.message_handler()
+def info(message):
+    if message.text.lower() == 'привет':
+        bot.send_message(message.chat.id, f"Привет! {message.from_user.first_name} {message.from_user.last_name}")
+    elif message.text.lower() == 'id':
+        bot.reply_to(message, f'ID: {message.from_user.id}')
+
+
+bot.polling(none_stop=True)
