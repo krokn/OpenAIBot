b3b1c4f4eb58c5525d0bb59fb2fdd5f13903bb1f
Vladislav Kalyuzhny <dalvvlad2004@mail.ru>
2023-10-25 13:22:08 +0300

    set button and processing photo

--- AI.py
+++ AI.py
@@ -1,7 +1,22 @@
 import telebot
+import webbrowser
+from telebot import types
 
 bot = telebot.TeleBot('6718573593:AAHVZng3giJHCTsO9m7ieF7qa3Z22Cghp3Y')
 
+@bot.message_handler(content_types=['photo'])
+def get_photo(message):
+    markup = types.InlineKeyboardMarkup()
+    btn1 = types.InlineKeyboardButton('Перейти на сайт', url='https://www.youtube.com')
+    btn2 = types.InlineKeyboardButton('Удалить фото', callback_data='delete_photo')
+    btn3 = types.InlineKeyboardButton('Изменить текст', callback_data='change_text')
+    markup.row(btn1)
+    markup.row(btn2,btn3)
+    bot.reply_to(message, 'Мерзость то какая', reply_markup=markup)
+
+@bot.message_handler(commands=['site','website'])
+def main(message):
+    webbrowser.open('https://www.youtube.com')
 
 
 @bot.message_handler(commands=['start'])
